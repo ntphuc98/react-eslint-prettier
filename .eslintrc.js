50cea24fedcabf2d0cccac5feab98cebcaccee83
Nguyen Thanh Phuc <ntphuc98it@gmail.com>
2022-03-13 12:13:08 +0700

    Add ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,3 @@
-### Create React App
-
-```
-npx create-react-app react-eslint-prettier --template typescript
-```
-
-### Setup ESLint
-
-- Step 1: Add ESLint package
-
-```
-npm install eslint --save-dev
-```
-
-or
-
-```
-yarn add eslint --dev
-```
-
-- Step 2: Create `.eslintrc.json` by running
-
-```
-npx eslint --init
-```
-
-or
-
-```
-yarn run eslint --init
-```
-
-![ESLint init](/public/eslint-init.gif)
-
-- Step 3: Change eslint config
-
-```
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
@@ -59,7 +22,6 @@ module.exports = {
     'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:storybook/recommended',
-    'plugin:valtio/recommended',
   ],
   plugins: [
     // ...
@@ -101,80 +63,3 @@ module.exports = {
     'max-params': ['warn'],
   },
 };
-```
-
-- Step 4: Add ESLint commands to `package.json`
-
-```
-"eslint": "eslint --cache \"src/**/*.{js,jsx,ts,tsx}\" \"functions/src/**/*.ts\"",
-"prettier": "prettier \"./**/*.{js,jsx,ts,tsx,css,yml,json,md}\"",
-"lint": "run-p lint:*",
-"lint:eslint": "yarn eslint",
-"lint:prettier": "yarn prettier --check",
-"lint:tsc": "tsc",
-"fix": "run-s fix:*",
-"fix:eslint": "yarn eslint --fix",
-"fix:prettier": "yarn prettier --write",
-```
-
-- Step 5: Create `tsconfig.eslint.json`
-
-```
-{
-  "extends": "./tsconfig.json",
-  "include": [
-    "src/**/*.js",
-    "src/**/*.jsx",
-    "src/**/*.ts",
-    "src/**/*.tsx",
-    ".eslintrc.js"
-  ],
-  "exclude": ["node_modules"]
-}
-
-```
-
-- Step 6: Add `npm-run-all` packages to run command `yarn run-p ....`
-
-```
-yarn add npm-run-all --dev
-```
-
-### Setup Prettier
-
-- Step 1: Add Prettier packages
-
-```
-npm install eslint-config-prettier eslint-plugin-prettier prettier --save-dev
-```
-
-or
-
-```
-yarn add eslint-config-prettier eslint-plugin-prettier prettier --dev
-```
-
-- Step 2: Create `.prettierrc.js` and paste the below code
-
-```
-module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 80,
-  tabWidth: 2,
-  endOfLine: 'auto',
-};
-```
-
-- Step 3: Create `.prettierignore` and paste the below code
-
-```
-build/
-public/
-**/coverage/
-**/node_modules/
-**/*.min.js
-*.config.js
-tsconfig.json
-```
